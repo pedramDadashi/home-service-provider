af4216ede0ac9d171ea8236cb434e35ca187c197
Pedram <pedadashii@gmail.com>
2023-08-07 01:05:06 +0330

    added interface ClientRepository

---
+++ src/main/java/ir/maktabsharif/homeservicephase2/repository/ClientRepository.java
@@ -0,0 +1,22 @@
+package ir.maktabsharif.homeservicephase2.repository;
+
+
+import ir.maktabsharif.homeservicephase2.entity.user.Client;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends UsersRepository<Client> {
+
+    Optional<Client> findByEmail(String email);
+
+    @Query(" update Client c set c.password = :newPassword where c.email = :email")
+    int editPassword(String email, String newPassword);
+
+    boolean existsByEmail(String email);
+
+    @Query("update Client c set c.credit = :newCredit where c.id = :email")
+    int updateCredit(String email, Long newCredit);
+
+}
