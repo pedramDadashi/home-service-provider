f769e514bd7bededc7483ed4a31068d82963a242
Pedram <pedadashii@gmail.com>
2023-08-07 01:21:01 +0330

    changed interface extends ClientRepository

--- src/main/java/ir/maktabsharif/homeservicephase2/repository/ClientRepository.java
+++ src/main/java/ir/maktabsharif/homeservicephase2/repository/ClientRepository.java
@@ -4,12 +4,11 @@ package ir.maktabsharif.homeservicephase2.repository;
 import ir.maktabsharif.homeservicephase2.entity.user.Client;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends UsersRepository<Client> {
-
-    Optional<Client> findByEmail(String email);
 
     @Query(" update Client c set c.password = :newPassword where c.email = :email")
     int editPassword(String email, String newPassword);
@@ -18,5 +17,8 @@ public interface ClientRepository extends UsersRepository<Client> {
 
     @Query("update Client c set c.credit = :newCredit where c.id = :email")
     int updateCredit(String email, Long newCredit);
+
+    @Override
+    Optional<Client> findByEmail(String email);
 
 }
