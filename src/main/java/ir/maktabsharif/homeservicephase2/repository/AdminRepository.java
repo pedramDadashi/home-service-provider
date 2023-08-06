9a1049593864dd75fc6221807a0edeaeab46eef0
Pedram <pedadashii@gmail.com>
2023-08-07 00:56:56 +0330

    added interface AdminRepository

---
+++ src/main/java/ir/maktabsharif/homeservicephase2/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package ir.maktabsharif.homeservicephase2.repository;
+
+import ir.maktabsharif.homeservicephase2.entity.user.Admin;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends UsersRepository<Admin> {
+
+}
