d3267c3035c9fb23087bfbd38b9f380e8003ca2e
Pedram <pedadashii@gmail.com>
2023-08-07 00:45:02 +0330

    changed interface BaseRepository extends to JpaRepository

---
+++ src/main/java/ir/maktabsharif/homeservicephase2/base/repository/BaseRepository.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif.homeservicephase2.base.repository;
+
+import ir.maktabsharif.homeservicephase2.base.entity.BaseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.io.Serializable;
+
+
+public interface BaseRepository<E extends BaseEntity<ID>, ID extends Serializable>
+        extends JpaRepository<E,ID> {
+
+
+}
