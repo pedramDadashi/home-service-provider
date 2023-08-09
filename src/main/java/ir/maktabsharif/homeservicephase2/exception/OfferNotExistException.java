ee46540d438a18d2316d13d548d179ad7d1369c4
Pedram <pedadashii@gmail.com>
2023-08-09 07:29:34 +0330

    added one class exception

---
+++ src/main/java/ir/maktabsharif/homeservicephase2/exception/OfferNotExistException.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.homeservicephase2.exception;
+
+public class OfferNotExistException extends RuntimeException {
+    public OfferNotExistException(String message) {
+        super(message);
+    }
+}
