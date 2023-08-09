2c1499b6b507d27a8bef9877b6a70f19bcc0375b
Pedram <pedadashii@gmail.com>
2023-08-09 07:11:03 +0330

    added three method to class Validation

--- src/main/java/ir/maktabsharif/homeservicephase2/util/Validation.java
+++ src/main/java/ir/maktabsharif/homeservicephase2/util/Validation.java
@@ -1,48 +1,57 @@
 package ir.maktabsharif.homeservicephase2.util;
 
-import ir.maktabsharif.homeservicephase2.exception.EmailFormatException;
-import ir.maktabsharif.homeservicephase2.exception.ImageFormatException;
-import ir.maktabsharif.homeservicephase2.exception.ImageSizeException;
-import ir.maktabsharif.homeservicephase2.exception.PasswordFormatException;
+import ir.maktabsharif.homeservicephase2.exception.*;
 
 import java.io.File;
 import java.util.regex.Pattern;
+
 
 public class Validation {
 
     private Validation() {
     }
 
+    public static boolean checkBlank(String str) {
+        if (str.isBlank()) throw new TextBlankException("the string input is blank!");
+        return true;
+    }
+
+    public static boolean checkText(String text) {
+        String textRegex = "^[a-zA-Z]*$";
+        if (!Pattern.matches(textRegex, text))
+            throw new AlphabetException("the wording of the text is not incorrect!");
+        return true;
+    }
+
+    public static boolean checkPositiveNumber(Long longDigit) {
+        if (longDigit < 0)
+            throw new PositiveNumberException("the number is negative!");
+        return true;
+    }
+
     public static boolean checkEmail(String email) {
         String emailRegex = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
-        if (Pattern.matches(emailRegex, email)) {
-            return true;
-        } else {
+        if (!Pattern.matches(emailRegex, email))
             throw new EmailFormatException("the format of the email is incorrect!");
-        }
-
+        return true;
     }
+
 
     public static boolean checkPassword(String password) {
         String passwordRegex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
-        if (Pattern.matches(passwordRegex, password)) {
-            return true;
-        } else {
+        if (!Pattern.matches(passwordRegex, password))
             throw new PasswordFormatException("the format of the password is incorrect!");
-        }
+        return true;
     }
 
     public static boolean checkImage(File image) {
         String imageName = image.getName();
+        checkBlank(imageName);
         int index = imageName.lastIndexOf('.');
-        if (index > 0 && imageName.substring(index + 1).equalsIgnoreCase("JPG")) {
-            if (image.length() <= 300000L) {
-                return true;
-            } else {
-                throw new ImageSizeException("the size of the image is bigger than 300kilo byte!");
-            }
-        } else {
+        if (!(index > 0 && imageName.substring(index + 1).equalsIgnoreCase("JPG")))
             throw new ImageFormatException("the format of the image is incorrect!");
-        }
+        if (image.length() > 300000L)
+            throw new ImageSizeException("the size of the image is bigger than 300kb!");
+        return true;
     }
 }
