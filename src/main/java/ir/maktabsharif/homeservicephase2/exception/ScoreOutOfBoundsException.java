037cb200c9b4732f9bf4279ae62d7f256b50be55
Pedram <pedadashii@gmail.com>
2023-08-30 00:29:54 +0330

    added exception class

---
+++ src/main/java/ir/maktabsharif/homeservicephase2/exception/ScoreOutOfBoundsException.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.homeservicephase2.exception;
+
+public class ScoreOutOfBoundsException extends RuntimeException {
+    public ScoreOutOfBoundsException(String message) {
+        super(message);
+    }
+}
