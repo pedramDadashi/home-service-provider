a3578bb2227f603cb68422441597012eb65d6027
Pedram <pedadashii@gmail.com>
2023-09-07 01:59:10 +0330

    added class CustomUserDetailsService

---
+++ src/main/java/ir/maktabsharif/homeservicephase2/service/Impl/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package ir.maktabsharif.homeservicephase2.service.Impl;
+
+import ir.maktabsharif.homeservicephase2.entity.user.Users;
+import ir.maktabsharif.homeservicephase2.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Users> users = usersRepository.findByEmail(username);
+        if (users.isEmpty())
+            throw new UsernameNotFoundException("not found!");
+        return users.get();
+    }
+}
