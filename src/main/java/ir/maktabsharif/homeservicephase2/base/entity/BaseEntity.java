4792e5ac48c7b8218a8cc6ab1ebe379cd3658689
Pedram <pedadashii@gmail.com>
2023-09-12 16:47:07 +0330

    added creationAt field in BaseEntity

--- src/main/java/ir/maktabsharif/homeservicephase2/base/entity/BaseEntity.java
+++ src/main/java/ir/maktabsharif/homeservicephase2/base/entity/BaseEntity.java
@@ -1,25 +1,27 @@
 package ir.maktabsharif.homeservicephase2.base.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.AUTO;
+import static lombok.AccessLevel.PRIVATE;
 
-
+@Data
 @MappedSuperclass
-@Getter
-@Setter
-
-public abstract class BaseEntity<ID extends Serializable>
-        implements Serializable {
+@FieldDefaults(level = PRIVATE)
+public abstract class BaseEntity<ID extends Serializable> implements Serializable {
     @Id
     @GeneratedValue(strategy = AUTO)
-    private ID id;
-
+    ID id;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    LocalDateTime registrationTime = LocalDateTime.now();
 }
+
---
+++ src/test/java/ir/maktabsharif/homeservicephase2/service/Impl/test.java
@@ -0,0 +1,26 @@
+//package ir.maktabsharif.homeservicephase2.service.Impl;
+//
+//import ir.maktabsharif.homeservicephase2.service.EmailService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.mail.SimpleMailMessage;
+//
+//@SpringBootTest
+//public class test {
+//
+//    @Autowired
+//    private EmailService emailService;
+//
+//    @Test
+//    void send() {
+//        SimpleMailMessage smm = new SimpleMailMessage();
+//        SimpleMailMessage mailMessage = new SimpleMailMessage();
+//        mailMessage.setTo("bondarali1380@gmail.com");
+//        mailMessage.setFrom("pedadashii@gmail.com");
+//        mailMessage.setSubject("Complete Customer Registration!");
+//        mailMessage.setText("To confirm your account, please click here :");
+//        emailService.sendEmail(smm);
+//    }
+//
+//}
