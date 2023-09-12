3e87e058180c75842aa46620f67ad23147301e33
Pedram <pedadashii@gmail.com>
2023-09-12 16:43:11 +0330

    added Exception AddressFormat

---
+++ src/main/java/ir/maktabsharif/homeservicephase2/exception/AddressFormatException.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.homeservicephase2.exception;
+
+public class AddressFormatException extends RuntimeException {
+    public AddressFormatException(String message) {
+        super(message);
+    }
+}
